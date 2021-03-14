3f229035354f16333d91fabc1c252032f7049ee0
CesarFCL <57689841+CesarFCL@users.noreply.github.com>
2021-03-13 22:10:33 -0500

    Bot-Telegram with Selenium V.2
    Configuración del 2do comando (BUYRT_TICKET)

--- mainTB.py
+++ mainTB.py
@@ -1,4 +1,5 @@
 import logging
+from time import sleep
 from selenium import webdriver
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -103,7 +104,136 @@ def list_command(update, context):
 
 #comando brt_ticket
 def BUYRT_TICKET_command(update, context):
-    update.message.reply_text('Aun no configurado')
+    busqueda = 1
+    if not context.args:
+        busqueda = 0
+        update.message.reply_text('Introduzca los parametros adecuados\n'
+                                  'Ejemplo: "/BUYRT_TICKET GYE AMS 2021-05-29 2021-06-16 3"\n'
+                                  'Para mas información use /help')
+    while busqueda == 1:
+        try:
+            update.message.reply_text('La busqueda de vuelos esta en proceso.\n'
+                                      'Este proceso puede tardar unos segundos'
+                                      ' debido a que las busquedas se realizan en tiempo'
+                                      ' real.\n'
+                                      'En cuanto se acabe de realizar el listado'
+                                      ' se le notificara con un mensaje.\n'
+                                      'Porfavor sea paciente')
+            origen = ''
+            destino = ''
+            Fsal = ''
+            Fret = ''
+            Nasientos = ''
+            try:
+                origen = context.args[0]
+                destino = context.args[1]
+                Fsal = context.args[2]
+                Fret = context.args[3]
+                Nasientos = context.args[4]
+            except:
+                update.message.reply_text('Error en los datos de busqueda')
+
+            # -------------------------------------------------
+
+            driver = webdriver.Chrome('./chromedriver.exe')
+
+            driver.get(
+                f'https://www.despegar.com.ec/shop/flights/results/roundtrip/{origen}/{destino}/{Fsal}/{Fret}/{Nasientos}/0/0/NA/NA/NA/NA/NA?from=SB&di={Nasientos}-0')
+
+            sleep(5)
+
+            for i in range(3):
+                try:
+                    driver.execute_script("window.scrollBy(0,4000);")
+                    sleep(4)
+                except:
+                    break
+
+            for i in range(2):
+                try:
+                    boton = driver.find_element_by_class_name('eva-3-btn-ghost.-lg')
+                    boton.click()
+                    sleep(4)
+                except:
+                    break
+
+            vuelos = driver.find_elements_by_class_name('eva-3-cluster-basic.-eva-3-shadow-line-hover')
+
+            listprecio = list()
+
+            for vuelo in vuelos:
+                precio = vuelo.find_element_by_class_name('amount.price-amount').text
+                listprecio.append(precio)
+                listprecio.append(precio)
+                # print(precio)
+            print("Precio de los vuelos:")
+            print(listprecio)
+
+            listida = list()
+            for vuelo in vuelos:
+                ida = vuelo.find_elements_by_class_name('city-departure.route-info-item.route-info-item-city-departure')
+                for i in ida:
+                    listida.append(i.text)
+
+            listreg = list()
+
+            for vuelo in vuelos:
+                reg = vuelo.find_elements_by_class_name('city-arrival.route-info-item.route-info-item-city-arrival')
+                for i in reg:
+                    listreg.append(i.text)
+                # print(reg)
+
+            listhoras = list()
+
+            for vuelo in vuelos:
+                Firsthora = vuelo.find_elements_by_class_name('itinerary-wrapper.-selected')
+                for x in Firsthora:
+                    hora = x.find_elements_by_class_name('hour')
+                    for i in hora:
+                        listhoras.append(i.text)
+
+            x = len(listhoras)
+
+            horaida = listhoras[0::2]
+            horareg = listhoras[1::2]
+            print("Horas de ida")
+            print(horaida)
+            print("Horas de regreso")
+            print(horareg)
+
+            listidavuelta = zip(listida, horaida, horareg, listreg, listprecio)
+            listidavuelta = tuple(listidavuelta)
+            print(len(listidavuelta))
+            tam = len(listidavuelta)
+            tam = tam / 2
+            print("Ciudad origen - Hora de salida- Hora de regreso - Ciudad destino")
+            print(listidavuelta)
+
+            update.message.reply_text("Boletos disponibles de compra:")
+            numeracion = 1
+            aux = 0
+            for i in range(int(tam)):
+                update.message.reply_text('\n' + str(numeracion) + '.\n- Fecha de salida: ' + str(Fsal) +
+                                          ' \n- Ciudad de partida: ' + str(listidavuelta[aux][0]) +
+                                          ' \n- Hora salida: ' + str(listidavuelta[aux][1]) +
+                                          ' \n- Destino: ' + str(listidavuelta[aux][3]) +
+                                          ' \n- Hora llegada: ' + str(listidavuelta[aux][2]) +
+                                          ' \n---------------------------------------------'
+                                          ' \n- Fecha de regreso: ' + str(Fret) +
+                                          ' \n- Ciudad de partida: ' + str(listidavuelta[aux + 1][0]) +
+                                          ' \n- Hora salida: ' + str(listidavuelta[aux + 1][1]) +
+                                          ' \n- Destino: ' + str(listidavuelta[aux + 1][3]) +
+                                          ' \n- Hora llegada: ' + str(listidavuelta[aux + 1][2]) +
+                                          ' \n---------------------------------------------'
+                                          ' \n- Numero de asientos: ' + str(Nasientos) +
+                                          ' \n- Precio: ' + str(listidavuelta[aux + 1][4]))
+                aux = aux + 2
+                numeracion = numeracion + 1
+            # -------------------------------------------------
+            busqueda = 0
+        except:
+            busqueda = 0
+            "Error en la busqueda"
 
 #comando b_ticket
 def BUY_TICKET_command(update, context):
